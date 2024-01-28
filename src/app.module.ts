937f9f0e1ab4551b20f47c214036f7b2a599504c
Samuel Trindade <samuel.junior.sjc@gmail.com>
2024-01-28 18:49:13 -0300

    "fix: module arch"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,34 +1,18 @@
 import { StocksModule } from './stocks/stocks.module';
-import { StocksController } from './stocks/stocks.controller';
 import { Module } from '@nestjs/common';
 import { FiisModule } from './fiis/fiis.module';
 import { CryptoModule } from './crypto/crypto.module';
-import { CryptoService } from './crypto/crypto.service';
-import { CryptoController } from './crypto/crypto.controller';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PrismaService } from './database/prisma.service';
-import { FiisService } from './fiis/fiis.service';
-import { FiisController } from './fiis/fiis.controller';
-import { StocksService } from './stocks/stocks.service';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
+import { ImagesModule } from './images/images.module';
+
+
 @Module({
-  imports: [StocksModule, FiisModule, CryptoModule],
-  controllers: [
-    StocksController,
-    FiisController,
-    CryptoController,
-    AppController,
-    UsersController,
-  ],
-  providers: [
-    AppService,
-    PrismaService,
-    FiisService,
-    StocksService,
-    CryptoService,
-    UsersService
+  imports: [
+    StocksModule, 
+    FiisModule, 
+    CryptoModule, 
+    UsersModule, 
+    ImagesModule
   ],
 })
 export class AppModule {}
